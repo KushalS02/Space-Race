cfc4c4c1d9edbf9b585f2a28da319700f32160c8
alexanderpham <alexpham66@gmail.com>
2023-02-02 10:46:52 -0700

    updated model.h
    added typedef structs

---
+++ model.h
@@ -0,0 +1,24 @@
+#ifndef MODEL_H
+#define MODEL_H
+
+typedef struct 
+{
+    /* data */
+
+};
+
+typedef struct Rocketship
+{
+    /* data */
+
+};
+
+typedef struct Asteroids
+{
+    /* data */
+    
+};
+
+
+
+#endif
