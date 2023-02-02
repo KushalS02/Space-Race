388281fa8e3351a40debb5e68ebb5147948affc5
alexanderpham <alexpham66@gmail.com>
2023-02-02 11:04:32 -0700

    Update RASTER.C

--- RASTER.C
+++ RASTER.C
@@ -10,15 +10,19 @@ void plotPixel(UINT8* base, int x, int y) {
 	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_WIDTH) {
 
 		*(base + y * 80 + (x >> 3)) |= 1 << 7 - (x & 7);
-		
+
 	}
 
 }
 
 void plotVerticalLine(UINT8* base, int x, int y, int height) {
 
+
+
 }
 
 void plotHorizontalLine(UINT8* base, int x, int y, int length) {
+
+	 
 
 }
