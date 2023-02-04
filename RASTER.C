d5204a8455fa97bebfd1d58e78f61ab0451fdcab
alexanderpham <alexpham66@gmail.com>
2023-02-03 20:41:35 -0700

    idk
    updated some stuff

--- RASTER.C
+++ RASTER.C
@@ -11,8 +11,40 @@ void plotPixel(UINT8* base, int x, int y) {
 
 void plotVerticalLine(UINT8* base, int x, int y, int height) {
 
+
+
 }
 
 void plotHorizontalLine(UINT8* base, int x, int y, int length) {
+
+	
+
+}
+
+void plotBitmap8(UINT8* base, int x, int y, UINT8* bitmap, int height) {
+
+
+
+}
+
+void plotBitmap16(UINT16* base, int x, int y, UINT16* bitmap, int height) {
+
+
+
+}
+
+void plotScreen(UINT32* base, UINT32* bitmap) {
+
+	int i;
+
+	for (i = 0; i < SCREEN_BYTE_SIZE; i++) {
+		*(base + 1) = bitmap[i];
+	}
+
+}
+
+void clearRegion(void* base, int x, int y, int width, int height) {
+
+
 
 }
--- TST_RAST.C
+++ TST_RAST.C
@@ -5,7 +5,10 @@ int main()
 {
 	void *base = Physbase();
 
+	plotScreen(base, 1000);
 	plotPixel(base, 300, 300);
+
+
 
 	return 0;
 }
