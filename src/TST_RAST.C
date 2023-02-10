e919d18623291a410426680ac7d1c376cdff6000
alexanderpham <alexpham66@gmail.com>
2023-02-09 19:46:33 -0700

    Update TST_RAST.C

--- src/TST_RAST.C
+++ src/TST_RAST.C
@@ -8,11 +8,12 @@ int main()
 
 	clearScreen(base);
 
-	plotPixel(base, 50, 50);
+	/* plotPixel(base, 50, 50); */
+
+	plotBitmap8(base, astrv1, 120, 120, ASTRV1_HEIGHT);
 
 	plotBitmap8(base, astrv2, 100, 100, ASTRV2_HEIGHT);
 
-	plotBitmap8(base, astrv1, 120, 120, ASTRV1_HEIGHT);
 
 	/*
 	plotRectangle(base, 0, 0, 640, 400);
