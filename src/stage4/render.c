c3cb936a0e02edb61abf5b9a189a186b0e11c0d5
alexanderpham <alexpham66@gmail.com>
2023-02-20 11:13:29 -0700

    rendering
    hmmm

--- src/stage4/render.c
+++ src/stage4/render.c
@@ -1,2 +1,31 @@
 #include render.h
 
+void renderRocketship(const Rocketship *rocketship, UINT32 *base) {
+
+    plotBitmap32(base, shipv2, rocketship->x, rocketship->y, SHIPV2_HEIGHT);
+
+}
+
+void renderAsteroid(const Asteroid *asteroid, UINT8 *base) {
+
+    
+
+}
+
+void renderScoreBox(const Scorebox *scorebox, UINT16 *base) {
+
+
+
+}
+
+void renderHighscoreBox(const HighscoreBox *highscore, UINT16 *base) {
+
+
+
+}
+
+void render(Model *model, UINT16 *base) {
+
+
+
+}
