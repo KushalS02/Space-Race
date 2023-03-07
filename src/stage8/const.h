2ba3a3f97512dbddfcef457ac3e007dbd56a7f06
alexanderpham <alexpham66@gmail.com>
2023-03-06 20:58:30 -0700

    added stage 8
    yes stage 8 lol

---
+++ src/stage8/const.h
@@ -0,0 +1,51 @@
+#ifndef CONST_H
+#define CONST_H
+
+#include "TYPES.H"
+
+/*
+Screen stuff
+*/
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 400
+#define SCREEN_BYTE_SIZE 8000
+
+/*
+Sprites Constants
+*/
+#define SHIPV2_HEIGHT 47
+#define ASTRV2_HEIGHT 8
+#define CHKLINE_HEIGHT 8
+
+/*
+Rocketship Constants 
+*/
+#define ROCKETSHIP_STARTING_X 316
+#define ROCKETSHIP_STARTING_Y 353
+#define ROCKETSHIP_SPEED 1 
+
+/*
+Asteroids Constants
+*/
+#define ASTEROID_SPEED 1
+#define ASTEROID_ROWS 20
+
+/*
+Scorebox Position
+*/
+#define SCOREBOX_X 10
+#define SCOREBOX_Y 0
+
+/*
+Highscore box Position
+*/
+#define HIGHSCOREBOX_X 518
+#define HIGHSCOREBOX_Y 0
+
+/*
+Keyboard key codes
+*/
+#define UP_KEY 0x00480000
+#define DOWN_KEY 0x00500000
+
+#endif
