ac91cb2108f2824c58e643bd605df18fc41c3691
alexanderpham <alexpham66@gmail.com>
2023-03-17 15:59:25 -0600

    fixing it here

---
+++ src/stage6 copy/const.h
@@ -0,0 +1,84 @@
+/*
+Authours: Alexander Pham and Kushal Saini
+*/
+#ifndef CONST_H
+#define CONST_H
+
+#include "TYPES.H"
+
+/*
+Screen stuff
+*/
+#define SCREEN_WIDTH 640
+#define MIN_XPOS 0
+#define WIDTH_BYTES 80
+#define SCREEN_HEIGHT 400
+#define HEIGHT_BYTES 25
+#define SCREEN_BYTE_SIZE 8000
+#define RECT_PLOT_X 0
+#define RECT_PLOT_Y 0
+
+/*
+Sprites Constants
+*/
+#define SHIPV2_HEIGHT 47
+#define SHIPV2_WIDTH 32
+#define ASTRV2_HEIGHT 8
+#define ASTRV2_WIDTH 8
+#define CHKLINE_HEIGHT 8
+
+/*
+Rocketship Constants 
+*/
+#define ROCKETSHIP_STARTING_X 303
+#define ROCKETSHIP_STARTING_Y 353
+#define ROCKETSHIP_SPEED 8
+
+/*
+Asteroids Constants
+*/
+#define ASTEROID_SPEED 2
+#define ASTEROID_MAX 20
+#define ASTEROID_STARTING_Y 32
+#define ASTEROID_BOX_SIZE 16 /* ACTUAL SIZE OF THE ASTEROID NOT HITBOX*/
+#define ASTEROID_SPACING 8
+#define ASTEROID_MAX_Y CHKLINE_HEIGHT + 4
+/*
+Scorebox Position
+*/
+#define SCOREBOX_X 10
+#define SCOREBOX_Y 0
+
+/*
+Highscore box Position
+*/
+#define HIGHSCOREBOX_X 525
+#define HIGHSCOREBOX_Y 0
+
+/*
+Score Stuff
+*/
+#define MAX_SCORE 999
+#define SCORE_INCREMENT 1
+
+/*
+Keyboard key codes
+*/
+#define UP_KEY 0x48
+#define DOWN_KEY 0x50
+#define ESC_KEY 0x01
+
+/*
+Other stuff
+*/
+#define SPRITE_SIZE 16
+#define SPRITE_HEIGHT 16
+#define SEED 1
+
+/*
+System stuff
+*/
+#define SYSTEM_CLOCK 0x462
+#define SCREEN_BUFFER_SIZE 32256
+
+#endif
---
+++ src/stage6 copy/events.h
@@ -0,0 +1,71 @@
+#ifndef EVENTS_H
+#define EVENTS_H
+
+#include <osbind.h>
+#include "model.h"
+
+/*
+* Function: rocketshipMove
+* Purpose: moves rocketship up or down depending on key press
+* Input:
+*   - Rocketship*: the rocketship to be moved
+*   - unsigned long: the code of the pressed key
+*/
+void rocketshipMove(Rocketship* rocketship, unsigned long key);
+
+/*
+* Function: rocketshipHitBoundry
+* Purpose: checks if the rocketship is hitting the bottom boundry
+*   and sets the bool hitBoundry member of Rocketship accordingly.
+* Input:
+*   - Rocketship*: the rocketship being checked
+*/
+void rocketshipHitBoundary(Rocketship* rocketship);
+
+/*
+* Function: rocketshipHitFinish
+* Purpose: checks if the rocketship has reached the finish line
+* Input:
+*   - Rocketship*: rocketship being checked
+* Output:
+*   - bool: False = has not reached finish, True = reached finish
+*/
+bool rocketshipHitFinish(Rocketship* rocketship);
+
+/*
+* Function: asteroidsHitBoundry
+* Purpose: If any of the asteroids in the array are at or past the screen boundry,
+*   the are reset to the other side of the screen
+* Input: 
+*   - Asteroid*: pointer to the array of asteroids
+*/
+void asteroidsHitBoundary(Asteroid* asteroid);
+
+/*
+* Funciton: rocketshipAsteroidCollision
+* Purpose: Checks for collisions between the rocketship and asteroids.
+*   If ther are any collisions, the gameOver flag in model is set to true.
+* Input:
+*   - Model*: the game model
+*/
+void rocketshipAsteroidCollision(Model *model);
+
+void onGameStart(Model* model);
+void onGamePause(Model* model);
+void onGameOver(Model* model);
+
+/*
+ Function hasUserInput
+ Purpose: check if a key is being pressed 
+ Output:
+    - bool: True = key is pressed, False = key not pressed
+*/
+bool hasUserInput();
+
+/*
+ Function: getUserInput
+ Purpose: get the key code that is being pressed
+*/
+unsigned long getUserInput();
+
+#endif
