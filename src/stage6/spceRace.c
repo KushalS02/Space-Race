4a66961fa6bd7774e7a97f96447645119dbfb5c4
alexanderpham <alexpham66@gmail.com>
2023-03-16 22:14:11 -0600

    no more warnings

--- src/stage6/spceRace.c
+++ src/stage6/spceRace.c
@@ -105,7 +105,7 @@ void processAsyncEvents(Model *model, void *base) {
 
             clearAsteroids(model->asteroids, base);
             clearRocketship(&model->player, base);
-            initializeNextRound(&model->player, &model->asteroids, &model->scorebox, &model->highscorebox);
+            initializeNextRound(&(model->player), model->asteroids, &(model->scorebox), &(model->highscorebox));
             renderRocketship(&model->player, base);
             renderNextRound(model, base);
         }
