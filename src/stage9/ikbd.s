7359a65e01be19e3c2af13a88af7d2bd355a28ea
alexanderpham <alexpham66@gmail.com>
2023-03-23 22:00:01 -0600

    my brain

---
+++ src/stage9/ikbd.s
@@ -0,0 +1,7 @@
+            xdef        _ikbdISR
+            xref        _ikbdReq
+
+_ikbdISR:   movem.l     d0/a0,-(sp)
+            jsr         _ikbdReq
+            movem.l     (sp)+,d0/a0
+            rte
