cdb50c17098c1a16997e891ae1662d9f35d48342
alexpham <alexpham66@gmail.com>
2023-04-05 22:03:50 -0600

    yes

--- src/stage8/psg.h
+++ src/stage8/psg.h
@@ -1,12 +1,8 @@
 /*
 Authours: Alexander Pham and Kushal Saini
-
 Course: COMP 2659 - 001 
-
 File name: psg.h
-
 Instructor: Paul Pospisil
-
 */
 #ifndef PSG_H
 #define PSG_H
@@ -15,20 +11,9 @@ Instructor: Paul Pospisil
 #include "TYPES.H"
 
 /*
-Valid ranges
+Check if the register of PSG is valid and in range
 */
-#define REG_MAX 0xF
-#define REG_MIN 0
-#define VAL_MAX 0xFF
-#define VAL_MIN 0
-#define NOISE_FREQ_MAX 31
-#define NOISE_FREQ_MIN 0
-#define TONE_MAX 0x0FFF
-#define TONE_MIN 0
-#define VOL_MAX 0xF
-#define VOL_MIN 0
-#define ENV_FREQ_MAX 0xFFFF
-#define ENV_FREQ_MIN 0
+#define regIsValid(reg) (reg >= 0 && reg <= 15)
 
 /*
 Addresses of PSG select and write 
@@ -37,186 +22,130 @@ extern volatile char* psgRegSelect = 0xFF8800;
 extern volatile char* psgRegWrite = 0xFF8802;
 
 /*
-Channel A, B, C tone registers
+Channel A, B, C tones
 */
-#define A_FINE_TONE 0x0
-#define B_FINE_TONE 0x2
-#define C_FINE_TONE 0x4
+#define CHANNEL_A_TONE 0x0
+#define CHANNEL_B_TONE 0x2
+#define CHANNEL_C_TONE 0x4
 
-#define A_ROUGH_TONE 0x1
-#define B_ROUGH_TONE 0x3
-#define C_ROUGH_TONE 0x5
+#define CHANNEL_A_ROUGH_TONE 0x1
+#define CHANNEL_B_ROUGH_TONE 0x3
+#define CHANNEL_C_ROUGH_TONE 0x5
 
 /*
-Channel A, B, C volume registers
+Channel A, B, C volume
 */
-#define A_VOL 0x8
-#define B_VOL 0x9
-#define C_VOL 0xa
+#define CHANNEL_A_VOL 0x8
+#define CHANNEL_B_VOL 0x9
+#define CHANNEL_C_VOL 0xa
 
 /*
-Mixer registers
-*/
-#define MIXER_REG 0x7
-
-/*
-Mixer masks
+Channels 
 */
 #define MIXER_ALL_OFF 0xff
-#define MIXER_TONE_A 0x3e
-#define MIXER_TONE_B 0x3d
-#define MIXER_TONE_C 0x3b
-#define MIXER_NOISE_A 0x37
-#define MIXER_NOISE_B 0x2f
-#define MIXER_NOISE_C 0x1f
+#define MIXER_TONE_CHANNEL_A 0x3e
+#define MIXER_TONE_CHANNEL_B 0x3d
+#define MIXER_TONE_CHANNEL_C 0x3b
+#define MIXER_NOISE_CHANNEL_A 0x37
+#define MIXER_NOISE_CHANNEL_B 0x2f
+#define MIXER_NOISE_CHANNEL_C 0x1f
 
 /*
-Envelope registers
+Envelope 
 */
-#define ENVELOPE_FINE 0xb
-#define ENVELOPE_ROUGH 0xc
-#define ENVELOPE_SHAPE 0xd
+#define ENVELOPE_FINE_REG 0xb
+#define ENVELOPE_ROUGH_REG 0xc
+#define ENVELOPE_SHAPE_CONTROL_REG 0xd
 
-/*
-Shape masks
-*/
-#define SAW_SHAPE 0xc
-#define TRIANGLE_SHAPE 0xe
+#define ENVELOPE_SAW_SHAPE 0xc
+#define ENVELOPE_SAW_SHAPE_INV 0x8
+#define ENVELOPE_SAW_PERIOD_SHAPE  0xf
+#define ENVELOPE_TRIANGLE_SHAPE 0xe
+#define ENVELOPE_TRIANGLE_INV_SHAPE 0xa
+#define ENVELOPE_TRIANGLE_PERIOD_SHAPE  0xd
+#define ENVELOPE_TRIANGLE_INV_PERIOD_SHAPE 0x00
 
 /* 
-Noise registers
+Regs
 */
-#define NOISE_FREQ 0x6
+#define NOISE_FREQUENCY_REG 0x6
+#define MIXER_REG 0x7
 
 /*
 Functions for PSG 
 */
 
 /*
-
     Function: writePSG
-
     Purpose: write the byte value to a PSG register 
-
     Parameters: 
-
-        UINT16 reg - the number of a PSG register that is
+        int reg - the number of a PSG register that is
                   being written to
-
         UINT8 val - the value that is written to the PSG
                     the register 
-
 */
-void writePSG(UINT16 reg, UINT8 val);
+void writePSG(int reg, UINT8 val);
 
 /*
-
     Function: readPSG
-
     Purpose: read the value in the given register
-
     Parameter:
-
-        UINT16 reg - the register number
-
+        int reg - the register number
     Returns: it returns the value that is stored in the 
              register 
                   
-
 */
-UINT8 readPSG(UINT16 reg);
+int readPSG(int reg);
 
 /*
-
     Function: setTone
-
-    Purpose: loads the tone registers for one of the channels
-             with the 12 bit tuning
-
-    Parameters:
-
-        int channel - the channel to set tone on
-
-        int tuning - the 12 bit tuning
-            - low 4 bits: rough adjustment
-            - bits 11-4: fine adjustment
-            - bits 15-12: unused
-
+    Purpose:
 */
-void setTone(Channel channel, UINT16 tuning);
+void setTone(int channel, int tuning);
 
 /*
-
     Function: setVolume
-
     Purpose: to load the volume register for one of
              the three channels 
-
     Parameters:
-
-        UINT16 channel - the channel to set the volume to
-
-        UINT16 volume - the volume value 
-
+        int channel - the channel to set the volume to
+        int volume - the volume value 
 */
-void setVolume(Channel channel, UINT16 volume);
+void setVolume(int channel, int volume);
 
 /*
-
     Function: enableChannel
-
     Purpose: to turn the given channel tone/noise on or off
-
     Parameters:
-
-        UINT16 channel - the channel to turn on/off the tone or noise
-
-        UINT16 toneOn - value on tone one
-
-        UINT16 noiseOn - value of noise on
-
+        int channel - the channel to turn on/off the tone or noise
+        int toneOn - value on tone one
+        int noiseOn - value of noise on
 */
-void enableChannel(Channel channel, bool toneOn, bool noiseOn);
+void enableChannel(int channel, int toneOn, int noiseOn);
 
 /*
-
     Function: stopSound
-
     Purpose: to stop all the sound on the PSG
-
 */
 void stopSound();
 
 /*
-
     Function: setEnvelope
-
     Purpose: loads the PSG envelope control register with
              one of the envelopes
-
     Parameters:
-
-        envelopeShape shape - the envelope shape type
-
+        envelopeShapeType shape - the envelope shape type
         UINT16 sustain - 16 bit value of the sustain 
-
 */
-void setEnvelope(envelopeShape shape, UINT16 sustain);
+void setEnvelope(envelopeShapeType shape, UINT16 sustain);
 
 /*
-
     Function: setNoise
-
     Purpose: load the register (noise) with the given 
              tuning value
-
     Parameters:
-
-        UINT16 tuning - the tuning value 
-            - Low 8 bits: fine adjustment
-            - High 8 bits: rought adjustment
-
+        int tuning - the tuning value 
 */
-void setNoise(UINT16 tuning);
+void setNoise(int tuning);
 
 #endif 
